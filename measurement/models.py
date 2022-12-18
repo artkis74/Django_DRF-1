3b7b95f348ce628cae233228abcaf31b76dedd7e
artkis74 <kiselev_artem1991@mail.ru>
2022-12-18 16:21:00 +0500

    add solving the task

--- measurement/models.py
+++ measurement/models.py
@@ -1,16 +1,22 @@
 from django.db import models
 
-# TODO: опишите модели датчика (Sensor) и измерения (Measurement)
-
 
 class Sensor(models.Model):
     name = models.CharField(max_length=50)
     description = models.CharField(max_length=100)
-    # measurement
+    # measurements
+
+    def __str__(self):
+        return self.description
 
 
 class Measurement(models.Model):
-    sensor = models.ForeignKey(Sensor, on_delete=models.CASCADE, related_name='sensor_meas')
+    sensor = models.ForeignKey(Sensor, on_delete=models.CASCADE, related_name='measurements')
     temper = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.created_at)
+
+
 
